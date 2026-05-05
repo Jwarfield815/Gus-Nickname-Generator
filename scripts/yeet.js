e0cec9eb006ae14a5e02c67a3e22d1eb587c9b65
Jwarfield815 <jonathanwarfield815@gmail.com>
2026-05-05 10:13:29 -0500

    got fontbomb working again

--- scripts/yeet.js
+++ scripts/yeet.js
@@ -1,6 +1,6 @@
 function yeetify() {
     document.getElementById("butt").style.transform = "scale(2,0)";
     var s = document.createElement('script');
-    s.setAttribute('src', 'http://fontbomb.ilex.ca/js/main.js');
+    s.setAttribute('src', 'scripts/fontbomb.js');
     document.body.appendChild(s);
 }
